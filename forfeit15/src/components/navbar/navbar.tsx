20481fd4112d90893c8d6e144b01709c48ad4ef1
Damian <damian.hoogsteder@indicia.nl>
2023-05-07 09:17:52 +0200

    feature: working notfications

--- forfeit15/src/components/navbar/navbar.tsx
+++ forfeit15/src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {HubConnection, HubConnectionBuilder, LogLevel} from '@microsoft/signalr';
 import {
     Box,
@@ -10,23 +10,40 @@ import {
     PopoverBody,
     PopoverContent,
     PopoverHeader,
-    PopoverTrigger,
+    PopoverTrigger, Text,
     useColorModeValue,
 } from '@chakra-ui/react';
-import { ArrowDownIcon, BellIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Link as RouterLink } from 'react-router-dom';
+import {ArrowDownIcon, BellIcon, SettingsIcon} from '@chakra-ui/icons';
+import {Link as RouterLink} from 'react-router-dom';
+
+interface PatchMessage {
+    Type: string;
+    UserId: string;
+    Title: string | null;
+    Description: string | null;
+    TimeStamp: string;
+}
+
+const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero if needed
+    const day = String(date.getDate()).padStart(2, '0'); // Add leading zero if needed
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+};
+
 
 export default function Navbar() {
     const [showNotifications, setShowNotifications] = useState(false);
     const [connection, setConnection] = useState<HubConnection | null>(null);
-    const [notifications, setNotifications] = useState<string[]>([]);
+    const [notifications, setNotifications] = useState<PatchMessage[]>([]);
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
             .withUrl('https://localhost:7088/updatehub')
             .withAutomaticReconnect()
             .build();
-        console.log(newConnection);
+        
         setConnection(newConnection);
     }, []);
 
@@ -34,17 +51,18 @@ export default function Navbar() {
         if (connection) {
             connection.start()
                 .then(result => {
-                    console.log('Connected!');
+                    const userId = "089d2e27-be98-4f8d-a3dc-1535127b0e9e"
+                    connection.invoke("Connect", userId);
 
                     connection.on('ReceiveMessage', message => {
-                        console.log(message)
-                        setNotifications([...notifications, message])
+                        const patchUpdate: PatchMessage = JSON.parse(message);
+                        setNotifications((prevNotifications) => [...prevNotifications, patchUpdate]);
                     });
                 })
                 .catch(e => console.log('Connection failed: ', e));
         }
     }, [connection]);
-    
+
     const handleNotificationsClick = () => {
         setShowNotifications(!showNotifications);
         if (showNotifications) {
@@ -56,7 +74,7 @@ export default function Navbar() {
     const color = useColorModeValue('white', 'white');
     const iconColor = useColorModeValue('gray.300', 'gray.400');
     const iconHoverColor = useColorModeValue('gray.500', 'gray.200');
-    
+
     return (
         <Flex
             bg={bg}
@@ -97,16 +115,34 @@ export default function Navbar() {
                     <PopoverContent bg={bg} color={color} border="none">
                         <PopoverArrow
                             bg={bg}
-                            _before={{content: '""', position: 'absolute', left: '-4px', top: '-4px', border: 'inherit'}}
-                            _after={{content: '""', position: 'absolute', left: '-2px', top: '-2px', border: 'inherit'}}
+                            _before={{
+                                content: '""',
+                                position: 'absolute',
+                                left: '-4px',
+                                top: '-4px',
+                                border: 'inherit'
+                            }}
+                            _after={{ content: '""', position: 'absolute', left: '-2px', top: '-2px', border: 'inherit' }}
                         />
                         <PopoverHeader>Notifications</PopoverHeader>
-                        <PopoverBody>
-                            {notifications.map((notificaition) => (
-                                <div>{notificaition}</div>
+                        <PopoverBody
+                            css={{
+                                maxHeight: '200px',
+                                overflowY: 'auto',
+                                scrollbarWidth: 'thin',
+                            }}
+                        >
+                            {notifications.map((notification) => (
+                                <div key={notification.TimeStamp} className="notification-item">
+                                    <Text as='b'>{notification.Type}</Text>
+                                    <Text>{notification.Title}</Text>
+                                    <Text>{notification.Description}</Text>
+                                    <Text>{formatTimestamp(notification.TimeStamp)}</Text>
+                                </div>
                             ))}
                         </PopoverBody>
                     </PopoverContent>
+
                 </Popover>
                 <Button
                     leftIcon={
